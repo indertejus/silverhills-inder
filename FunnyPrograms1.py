e08afa3f9a445fd932c925c0674769cfdf457c2f
Inder Tejus <indertejus@gmail.com>
2024-12-08 20:58:03 +0530

    YOKOSO, WATASHI NO SOUL SOCIETY 🗣️🔥🔥🔥🔥🔥🔥

--- FunnyPrograms1.py
+++ FunnyPrograms1.py
@@ -1,5 +1,5 @@
 def pause():
-    prgmPause = input("Press the <ENTER> key to continue...")
+    prgmPause = input("\nPress the <ENTER> key to continue...\n")
 
 list1 = list(input("Enter something:"))
 confirm = int(input("Do you want to print the list? Yes - 1 / No - 0 : "))
@@ -10,7 +10,22 @@ if confirm == yes:
 elif confirm == no:
     print("Exiting...")
 else: print("What the hell bro?")
+pause()
 
 for i in list1:
     print(i)
+pause()
+
+list2 = [0,1,2,3,4,5,6,7,8,9,0]
+sum = 0
+for i in list2:
+    sum = sum+i
+print("The elements in the list are:", list2)
+print("The sum is:", sum)
+pause()
+
+print("\n")
+for kek in list1:
+    print(kek)
+print("\n", list1[-1],list1[5], list1[-3],list1[0])
 pause()
